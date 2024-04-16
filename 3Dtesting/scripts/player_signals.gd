a9817b4dde0b5c7a33197a03892615f7dcdaf855
Ryxga <osmanuzun362@gmail.com>
2024-04-16 20:09:35 +0200

    eimer freeze

--- 3Dtesting/scripts/player_signals.gd
+++ 3Dtesting/scripts/player_signals.gd
@@ -7,5 +7,6 @@ signal concrete_bucket_pos_reached
 func _on_concrete_bucket_pos_body_entered(body):
 	if body.is_in_group("concretebucket"):
 		body.remove_from_group("grab")
+		body.freeze = true
 		body.position = concrete_bucket_spawn.global_position
 		emit_signal("concrete_bucket_pos_reached")
