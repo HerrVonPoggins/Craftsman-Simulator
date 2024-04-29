532304a01cabffd02db7d9bbc568f24f8a5cad68
Ryxga <osmanuzun362@gmail.com>
2024-04-29 09:53:14 +0200

    sand üprogress bar funktioniert
    man muss nur noch ein Objekt in die CorePlayable Szene einfügen, welches in der Gruppe "sand" ist

--- 3Dtesting/mechanics/cement-mixing/zementmischer.gd
+++ 3Dtesting/mechanics/cement-mixing/zementmischer.gd
@@ -3,6 +3,8 @@ var water = false
 var concrete = false
 var water_filled = false
 var cement_filled = false
+var sand = false
+var sand_filled = false
 
 @onready var concrete_bucket_obj = $"../Props/ConcreteBucketObj"
 @onready var collision_shape_3d = $"../Props/ConcreteBucketObj/CollisionShape3D"
@@ -28,6 +30,12 @@ func _process(delta):
 	if cement_value.value >= 100:
 		cement_value.value = 100
 		cement_filled = true
+	if sand == true:
+		if Input.is_action_pressed("leftclick"):
+			sand_value.value += 1.5
+	if sand_value.value >= 100:
+		sand_value.value = 100
+		sand_filled = true
 	#mechanic to fill water and concretebag into the mixer
 func _on_range_body_entered(body):
 	if body.is_in_group("water"):
@@ -36,10 +44,12 @@ func _on_range_body_entered(body):
 	if body.is_in_group("concrete"):
 		cement_value.visible = true
 		concrete = true
-
+	if body.is_in_group("sand"):
+		sand_value.visible = true
+		sand = true
 
 func _on_player_start_mixer():
-	if water_filled == true and cement_filled == true:
+	if water_filled == true and cement_filled == true and sand_filled == true:
 		#if mixer is turned on the concrete bucket becomes visible 
 		concrete_bucket_obj.visible = true
 		concrete_bucket_obj.freeze = false
@@ -56,3 +66,6 @@ func _on_range_body_exited(body):
 	if body.is_in_group("concrete"):
 		cement_value.visible = false
 		concrete = false
+	if body.is_in_group("sand"):
+		sand_value.visible = false
+		sand = false
