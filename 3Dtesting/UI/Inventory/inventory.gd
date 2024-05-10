3fd0607817d2d4f763a25b4d250d1eba16837ab8
Osman Uzun <147635130+Ryxga@users.noreply.github.com>
2024-05-10 03:29:22 +0200

    added inventory to player scene
    added a canvaslayer so the inventory now appears ingame.

--- 3Dtesting/UI/Inventory/inventory.gd
+++ 3Dtesting/UI/Inventory/inventory.gd
@@ -1,5 +1,10 @@
 extends Control
 
+signal trowel_button_clicked
+
+var player_raycast = "res://mechanics/Player/RayCast3D.gd"
+var trowel_icon = "res://assets/textures/icon.svg"
+
 @onready var trowel_button = $HBoxContainer/TrowelButton
 
 # Called when the node enters the scene tree for the first time.
@@ -12,4 +17,8 @@ func _process(delta):
 	if Input.is_action_just_pressed("inventory"):
 		self.visible = !self.visible
 	if Global.trowel_picked == true:
-		trowel_button.Icon = "res://assets/textures/icon.svg"
+		trowel_button.Icon = trowel_icon
+
+
+func _on_trowel_button_pressed():
+	emit_signal("trowel_button_clicked")
