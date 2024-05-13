2e9451d6f90376c9e1b1a9dc43f5c5e084c61295
Ryxga <osmanuzun362@gmail.com>
2024-05-13 11:37:30 +0200

    changing mesh script
    when planer enters wallguide body, the mesh changes

--- 3Dtesting/scenes/Carpenter/carpenter_world.gd
+++ 3Dtesting/scenes/Carpenter/carpenter_world.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var planed_mesh_wallguide = preload("res://assets/carpenter-assets/wallguide_mesh(planed).tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_wallguide_body_entered(body):
+	if body.is_in_group("planer"):
+		$Props/Wallguide/Wallguide.Mesh = planed_mesh_wallguide
