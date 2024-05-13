39e5555ed431ea72087cd8a9218ec8f8efc868ea
Ryxga <osmanuzun362@gmail.com>
2024-05-14 01:10:43 +0200

    changed position of the bricks

--- 3Dtesting/mechanics/brick_cutting/brick_cutting.gd
+++ 3Dtesting/mechanics/brick_cutting/brick_cutting.gd
@@ -6,13 +6,13 @@ func _on_cutting_area_area_entered(area):
 	if area.is_in_group("cutting_point"):
 		var brick_half1 = brick_scene.instantiate()
 		brick_half1.position = Vector2(810, 350)
-		var sprite_1 = brick_half1.get_node("Sprite2D")
+		var sprite_1 = brick_half1.get_child(0)
 		sprite_1.region_enabled = true
 		sprite_1.region_rect = Rect2(-sprite_1.texture.get_width()/2, 0.0, sprite_1.texture.get_width(), sprite_1.texture.get_height())
 		add_child(brick_half1)
 		var brick_half2 = brick_scene.instantiate()
 		brick_half2.position = Vector2(790, 350)
-		var sprite_2 = brick_half2.get_node("Sprite2D")
+		var sprite_2 = brick_half2.get_child(0)
 		sprite_2.region_enabled = true
 		sprite_2.region_rect = Rect2(sprite_2.texture.get_width()/2, 0.0, sprite_2.texture.get_width(), sprite_2.texture.get_height())
 		add_child(brick_half2)
