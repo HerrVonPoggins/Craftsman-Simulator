1143754be80ac2e168ab4150cdaca57bf35531a6
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2024-05-13 14:38:07 +0200

    powerbox fix

--- 3Dtesting/mechanics/PowerBox/power_box.gd
+++ 3Dtesting/mechanics/PowerBox/power_box.gd
@@ -25,23 +25,64 @@ func _on_outlet_range_2_body_exited(body):
 
 
 func _on_outlet_range_3_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("wire"):
+		Global.let_go = true
+		body.freeze = true
+		await get_tree().create_timer(0.5).timeout
+		body.position = $Outlet3/OutletRange3.global_position
+		body.rotation_degrees = Vector3(90,0,0)
 
+func _on_outlet_range_3_body_exited(body):
+	if body.is_in_group("wire"):
+		body.freeze = false
 
 func _on_outlet_range_4_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("wire"):
+		Global.let_go = true
+		body.freeze = true
+		await get_tree().create_timer(0.5).timeout
+		body.position = $Outlet4/OutletRange4.global_position
+		body.rotation_degrees = Vector3(90,0,0)
 
+func _on_outlet_range_4_body_exited(body):
+	if body.is_in_group("wire"):
+		body.freeze = false
 
 func _on_outlet_range_5_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("wire"):
+		Global.let_go = true
+		body.freeze = true
+		await get_tree().create_timer(0.5).timeout
+		body.position = $Outlet5/OutletRange5.global_position
+		body.rotation_degrees = Vector3(90,0,0)
 
+func _on_outlet_range_5_body_exited(body):
+	if body.is_in_group("wire"):
+		body.freeze = false
 
 func _on_outlet_range_6_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("wire"):
+		Global.let_go = true
+		body.freeze = true
+		await get_tree().create_timer(0.5).timeout
+		body.position = $Outlet6/OutletRange6.global_position
+		body.rotation_degrees = Vector3(90,0,0)
+
+func _on_outlet_range_6_body_exited(body):
+	if body.is_in_group("wire"):
+		body.freeze = false
 
 
 func _on_outlet_range_7_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("wire"):
+		Global.let_go = true
+		body.freeze = true
+		await get_tree().create_timer(0.5).timeout
+		body.position = $Outlet7/OutletRange7.global_position
+		body.rotation_degrees = Vector3(90,0,0)
 
+func _on_outlet_range_7_body_exited(body):
+	if body.is_in_group("wire"):
+		body.freeze = false
 
 
