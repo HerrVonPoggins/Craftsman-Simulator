cd788b8b600f295bb4ec7699c3ad7dc8376f0760
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2024-05-23 10:05:46 +0200

    functions for music

--- 3Dtesting/scripts/music.gd
+++ 3Dtesting/scripts/music.gd
@@ -13,3 +13,50 @@ func _process(delta):
 
 func _play_collect():
 	$Collect.play()
+
+func _play_main_menu():
+	$MaineMenu.play()
+func _stop_main_menu():
+	$MaineMenu.stop()
+func _pause_main_menu():
+	$MaineMenu.stream_paused = true
+func _continue_main_menu():
+	$MaineMenu.stream_paused = false
+
+func _play_button():
+	$ButtonSound.play()
+
+func _play_brick_placing():
+	$BrickPlacing.play()
+
+func _play_Concrete_Placing():
+	$ConcretePlacing.play()
+
+func _play_walk_floor():
+	$WalkFloor.play()
+
+func _play_walk_grass():
+	$WalkGrass.play()
+
+func _play_fill_mixer_concrete():
+	$FillMixerConcrete.play()
+
+func _play_fill_mixer_water():
+	$FillMixerWater.play()
+
+func _play_fill_mixer_sand():
+	$FillMixerSand.play()
+
+func _play_Wedge():
+	$WedgePlacing.play()
+
+func _play_mixer():
+	$MixerSound.play()
+
+func _play_saw():
+	$SawSound.play()
+
+
+
+
+
