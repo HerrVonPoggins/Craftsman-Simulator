d12d9922be4f40f9277372ce9012aa89b23798ed
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2024-05-28 16:38:34 +0200

    switch minigame begin

--- 3Dtesting/mechanics/CurcuitMinigame/curcuitcollision.gd
+++ 3Dtesting/mechanics/CurcuitMinigame/curcuitcollision.gd
@@ -1,7 +1,7 @@
 extends RayCast3D
 
 var obj = null
-
+var done = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,11 +9,52 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if $"../../../StaticBody3D2".rotation_degrees.x == -40 and done == false:
+		$"../../../Node3D".rotate_z(deg_to_rad(20))
+
+	elif $"../../../StaticBody3D2".rotation_degrees.x == 20:
+		$"../../../Node3D".rotate_z(deg_to_rad(-20))
+
+
+	if $"../../../StaticBody3D3".rotation_degrees.x == -40:
+		$"../../../Node3D".rotate_z(deg_to_rad(20))
+
+	elif $"../../../StaticBody3D3".rotation_degrees.x == 20:
+		$"../../../Node3D".rotate_z(deg_to_rad(-20))
+
+
+	if $"../../../StaticBody3D4".rotation_degrees.x == -40:
+		$"../../../Node3D".rotate_z(deg_to_rad(20))
+
+	elif $"../../../StaticBody3D4".rotation_degrees.x == 20:
+		$"../../../Node3D".rotate_z(deg_to_rad(-20))
+
+
+	if $"../../../StaticBody3D5".rotation_degrees.x == -40:
+		$"../../../Node3D".rotate_z(deg_to_rad(20))
+
+	elif $"../../../StaticBody3D5".rotation_degrees.x == 20:
+		$"../../../Node3D".rotate_z(deg_to_rad(-20))
+
+
+
+
+
+	
 	if Input.is_action_just_pressed("leftclick"):
 		var collider = get_collider()
-		
+
 
 		if collider != null:
 			if collider.is_in_group("circuit"):
-				print(collider.rotation)
+
 				collider.rotate_z(deg_to_rad(90.0))
+			if collider.is_in_group("switch"):
+				if collider.rotation.x >= 0:
+					collider.rotation.x = deg_to_rad(-40)
+				else:
+					collider.rotation.x = deg_to_rad(20)
+
+
+	if Input.is_action_just_pressed("kamera"):
+		print($"../../../StaticBody3D3".rotation_degrees.x)
