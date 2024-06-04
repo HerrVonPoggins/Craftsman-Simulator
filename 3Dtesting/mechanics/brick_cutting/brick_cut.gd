41f2607e051afa8085b68b4a8eef46e0de8e409a
Osman Uzun <147635130+Ryxga@users.noreply.github.com>
2024-06-04 17:03:03 +0200

    brick_cut script changed

--- 3Dtesting/mechanics/brick_cutting/brick_cut.gd
+++ 3Dtesting/mechanics/brick_cutting/brick_cut.gd
@@ -1,26 +1,32 @@
 extends Camera3D
 
-signal brick_clicked
-
-var mouse_click = false
-var mouse = Vector2()
-@onready var camera = $"."
-
-const RAY_LENGTH = 100.0
+var lock_in_wrong = false
+var lock_in_correct = false
 
 func _physics_process(delta):
-	if mouse_click == true:
-		var worldspace = get_world_3d().direct_space_state
-		var start = camera.project_ray_origin(mouse)
-		var end = start + camera.project_ray_normal(mouse) * RAY_LENGTH
-		var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(start, end))
-		#if result.collider == $"../BrickCut":
-			#pass
+	#Cuttable Brick Movement
+	$"../BrickCut".velocity = clamp($"../BrickCut".velocity, Vector3(0,0,-1), Vector3(0,0,1))
+	if Input.is_action_just_pressed("left"):
+		$"../BrickCut".velocity += Vector3(0,0,1)
+	if Input.is_action_just_pressed("right"):
+		$"../BrickCut".velocity += Vector3(0,0,-1)
+	$"../BrickCut".move_and_slide()
 
-func _input(event):
-	if event is InputEventMouse:
-		mouse = event.position
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			mouse_click = true
+	#Cutting the Brick at the marked position
+	if Input.is_action_just_pressed("interagieren"):
+		if lock_in_correct == true:
+			print("yessir")
+		elif lock_in_wrong == true:
+			print("wrong")
 
+func _on_cut_area_area_entered(area):
+	if area.is_in_group("lock_in"):
+		lock_in_wrong = true
+	if area.is_in_group("lock_in_correct"):
+		lock_in_correct = true
+
+
+func _on_cut_area_area_exited(area):
+	if area.is_in_group("lock_in") or area.is_in_group("lock_in_correct"):
+		lock_in_wrong = false
+		lock_in_correct = false
