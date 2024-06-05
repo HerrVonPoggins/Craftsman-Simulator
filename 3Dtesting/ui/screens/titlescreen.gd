3f7ea0853861b19db4f488b787a11020a4cdc088
Ciwler <112726219+Ciwler@users.noreply.github.com>
2024-06-05 03:54:18 +0200

    Title_screen

--- 3Dtesting/ui/screens/titlescreen.gd
+++ 3Dtesting/ui/screens/titlescreen.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 func _ready():
 	$AnimationPlayer.play("fade_in")
+	$AnimationPlayer.play("Logo")
 	
 	if Global.current_scene != -1:
 		Music._play_main_menu()
@@ -21,3 +22,19 @@ func _on_button_credits_pressed():
 
 func _on_button_quit_pressed():
 	get_tree().quit()
+
+
+func _on_button_start_mouse_entered():
+	$StartPlayer.play("Start_hover")
+
+
+func _on_button_quit_mouse_entered():
+	$QuitPlayer.play("Quit_hover")
+
+
+func _on_button_start_mouse_exited():
+	$StartPlayer.play("Start_quit")
+
+
+func _on_button_quit_mouse_exited():
+	$QuitPlayer.play("Quit_exit")
