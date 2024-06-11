6aaf613427980f0135d0c87dd4131966f5c6d758
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2024-06-11 22:29:15 +0200

    filling animations

--- 3Dtesting/mechanics/craneminigame/concrete_filler.gd
+++ 3Dtesting/mechanics/craneminigame/concrete_filler.gd
@@ -13,11 +13,13 @@ func _process(delta):
 
 
 	if Input.is_action_pressed("leftclick") and $".".visible == true and done == false:
-		$AnimationPlayer.play("fill")
+		$"Root Scene4/AnimationPlayer".play("Cylinder|CylinderAction")
+		$"Root Scene4/AnimationPlayer2".play("Icosphere_001|IcosphereAction_001")
 		$"Root Scene".position.y += 0.002
 		
 	if Input.is_action_just_released("leftclick") and $".".visible == true:
-		$AnimationPlayer.stop()
+		$"Root Scene4/AnimationPlayer".stop()
+		$"Root Scene4/AnimationPlayer2".stop()
 
 	if $"Root Scene".position.y >= -16.15:
 		done = true
