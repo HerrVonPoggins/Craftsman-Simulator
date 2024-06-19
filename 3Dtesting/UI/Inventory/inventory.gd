93a8ff2a31b618532bc191168e7951277c544834
Osman Uzun <147635130+Ryxga@users.noreply.github.com>
2024-06-19 11:02:06 +0200

    inventory button sounds played

--- 3Dtesting/UI/Inventory/inventory.gd
+++ 3Dtesting/UI/Inventory/inventory.gd
@@ -8,3 +8,11 @@ func _process(delta):
 		trowel_button.disabled = false
 	if Global.spirit_level_picked == true:
 		spirit_level_button.disabled = false
+
+
+func _on_trowel_button_pressed():
+	Music._play_button()
+
+
+func _on_spirit_level_button_pressed():
+	Music._play_button()
