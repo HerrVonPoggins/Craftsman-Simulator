77bd86f05ab589cbaf21829ed746d4f19cf901b1
Ryxga <osmanuzun362@gmail.com>
2024-06-25 03:04:48 +0200

    added camera rotation for second part of stairs

--- 3Dtesting/mechanics/Carpenter-Areas/stairs.gd
+++ 3Dtesting/mechanics/Carpenter-Areas/stairs.gd
@@ -2,6 +2,25 @@ extends Node3D
 
 @onready var animation_player = $AnimationPlayer
 var placed_step_counter = 0
+var camera_rotated = false
+
+func _process(delta):
+	if dübel_amount_1 >= 6:
+		await get_tree().create_timer(1).timeout
+		animation_player.play("Camera1")
+		dübel_amount_1 = 0
+		camera_rotated = true
+	
+	if camera_rotated == true:
+		if Input.is_action_pressed("left"):
+			$CameraStand/Camera1.position.x -= 2 * delta
+		if Input.is_action_pressed("right"):
+			$CameraStand/Camera1.position.x += 2 * delta
+		if Input.is_action_pressed("back"):
+			$CameraStand/Camera1.position.y -= 2 * delta
+		if Input.is_action_pressed("forward"):
+			$CameraStand/Camera1.position.y += 2 * delta
+
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("wallguide"):
@@ -27,17 +46,45 @@ func _on_area_3d_body_entered(body):
 		if placed_step_counter == 8:
 			$Step8.visible = true
 
-
+var dübel_amount_1 = 0
 func _on_dübel_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				animation_player.play("Dübel1")
-
-
+				dübel_amount_1 += 1
 
 func _on_dübel_2_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				animation_player.play("Dübel2")
+				dübel_amount_1 += 1
+
+func _on_dübel_3_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				animation_player.play("Dübel3")
+				dübel_amount_1 += 1
+
+func _on_dübel_4_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				animation_player.play("Dübel4")
+				dübel_amount_1 += 1
+
+func _on_dübel_5_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				animation_player.play("Dübel5")
+				dübel_amount_1 += 1
+
+func _on_dübel_6_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				animation_player.play("Dübel6")
+				dübel_amount_1 += 1
