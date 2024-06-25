75f9c7b0eae1079dd964c0d4dc0a8941b8e0639b
Ryxga <osmanuzun362@gmail.com>
2024-06-25 16:58:40 +0200

    dübel minigame done

--- 3Dtesting/mechanics/Carpenter-Areas/stairs.gd
+++ 3Dtesting/mechanics/Carpenter-Areas/stairs.gd
@@ -65,6 +65,8 @@ func _on_dübel_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel1")
 				dübel_amount_1 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel/Outline".queue_free()
 
 func _on_dübel_2_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -72,6 +74,8 @@ func _on_dübel_2_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel2")
 				dübel_amount_1 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel2/Outline2".queue_free()
 
 func _on_dübel_3_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -79,6 +83,8 @@ func _on_dübel_3_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel3")
 				dübel_amount_1 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel3/Outline3".queue_free()
 
 func _on_dübel_4_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -86,6 +92,8 @@ func _on_dübel_4_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel4")
 				dübel_amount_1 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel4/Outline4".queue_free()
 
 func _on_dübel_5_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -93,6 +101,8 @@ func _on_dübel_5_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel5")
 				dübel_amount_1 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel5/Outline5".queue_free()
 
 func _on_dübel_6_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -100,6 +110,8 @@ func _on_dübel_6_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel6")
 				dübel_amount_1 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel6/Outline6".queue_free()
 
 var dübel_amount_2 = 6
 func _on_dübel_7_input_event(camera, event, position, normal, shape_idx):
@@ -108,6 +120,8 @@ func _on_dübel_7_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel7")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel7/Outline7".queue_free()
 
 func _on_dübel_8_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -115,6 +129,8 @@ func _on_dübel_8_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel8")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel8/Outline8".queue_free()
 
 
 func _on_dübel_9_input_event(camera, event, position, normal, shape_idx):
@@ -123,6 +139,8 @@ func _on_dübel_9_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel9")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel9/Outline9".queue_free()
 
 
 func _on_dübel_10_input_event(camera, event, position, normal, shape_idx):
@@ -131,6 +149,8 @@ func _on_dübel_10_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel10")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel10/Outline10".queue_free()
 
 
 func _on_dübel_11_input_event(camera, event, position, normal, shape_idx):
@@ -139,6 +159,8 @@ func _on_dübel_11_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel11")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel11/Outline11".queue_free()
 
 
 func _on_dübel_12_input_event(camera, event, position, normal, shape_idx):
@@ -147,6 +169,8 @@ func _on_dübel_12_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel12")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel12/Outline12".queue_free()
 
 
 func _on_dübel_13_input_event(camera, event, position, normal, shape_idx):
@@ -155,6 +179,8 @@ func _on_dübel_13_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel13")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel13/Outline13".queue_free()
 
 
 func _on_dübel_14_input_event(camera, event, position, normal, shape_idx):
@@ -163,6 +189,8 @@ func _on_dübel_14_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel14")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel14/Outline14".queue_free()
 
 
 func _on_dübel_15_input_event(camera, event, position, normal, shape_idx):
@@ -171,6 +199,8 @@ func _on_dübel_15_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel15")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel15/Outline15".queue_free()
 
 
 func _on_dübel_16_input_event(camera, event, position, normal, shape_idx):
@@ -179,6 +209,8 @@ func _on_dübel_16_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel16")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel16/Outline16".queue_free()
 
 
 func _on_dübel_17_input_event(camera, event, position, normal, shape_idx):
@@ -187,6 +219,8 @@ func _on_dübel_17_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel17")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel17/Outline17".queue_free()
 
 
 func _on_dübel_18_input_event(camera, event, position, normal, shape_idx):
@@ -195,6 +229,8 @@ func _on_dübel_18_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel18")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel18/Outline18".queue_free()
 
 
 func _on_dübel_19_input_event(camera, event, position, normal, shape_idx):
@@ -203,6 +239,8 @@ func _on_dübel_19_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel19")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel19/Outline19".queue_free()
 
 
 func _on_dübel_20_input_event(camera, event, position, normal, shape_idx):
@@ -211,6 +249,8 @@ func _on_dübel_20_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel20")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel20/Outline20".queue_free()
 
 
 func _on_dübel_21_input_event(camera, event, position, normal, shape_idx):
@@ -219,6 +259,8 @@ func _on_dübel_21_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel21")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel21/Outline21".queue_free()
 
 
 func _on_dübel_22_input_event(camera, event, position, normal, shape_idx):
@@ -227,6 +269,8 @@ func _on_dübel_22_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel22")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel22/Outline22".queue_free()
 
 
 func _on_dübel_23_input_event(camera, event, position, normal, shape_idx):
@@ -235,6 +279,8 @@ func _on_dübel_23_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel23")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel23/Outline23".queue_free()
 
 
 func _on_dübel_24_input_event(camera, event, position, normal, shape_idx):
@@ -243,6 +289,8 @@ func _on_dübel_24_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel24")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel24/Outline24".queue_free()
 
 
 func _on_dübel_25_input_event(camera, event, position, normal, shape_idx):
@@ -251,6 +299,8 @@ func _on_dübel_25_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel25")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel25/Outline25".queue_free()
 
 
 func _on_dübel_26_input_event(camera, event, position, normal, shape_idx):
@@ -259,6 +309,8 @@ func _on_dübel_26_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel26")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel26/Outline26".queue_free()
 
 
 func _on_dübel_27_input_event(camera, event, position, normal, shape_idx):
@@ -267,6 +319,8 @@ func _on_dübel_27_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel27")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel27/Outline27".queue_free()
 
 
 func _on_dübel_28_input_event(camera, event, position, normal, shape_idx):
@@ -275,3 +329,5 @@ func _on_dübel_28_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				animation_player.play("Dübel28")
 				dübel_amount_2 += 1
+				await  animation_player.animation_finished
+				$"DübelContainer/Dübel28/Outline28".queue_free()
